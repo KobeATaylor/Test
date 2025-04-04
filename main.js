8d2f0f93b1aec2c5405596d10a56a0bfc2e81309
Kobe Taylor <ktaylor2@linfield.edu>
2025-04-04 13:15:10 -0700

    Replace play in amountFor function with playFor function call

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ function playFor(aPerformance)
 function amountFor(aPerformance,play)
 {
   let thisAmount = 0;
-  switch (play.type) {
+  switch (playFor(aPerformance).type) {
     case 'tragedy':
       thisAmount = 40000;
       if (aPerformance.audience > 30) {
@@ -118,7 +118,7 @@ function amountFor(aPerformance,play)
       thisAmount += 300 * aPerformance.audience;
       break;
     default:
-      throw new Error(`unknown type: ${play.type}`);
+      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
   }
   return thisAmount;
 }
