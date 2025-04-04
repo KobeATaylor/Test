f3557085f911ff08695b1b540ea44ee52370c584
Kobe Taylor <ktaylor2@linfield.edu>
2025-04-04 13:58:03 -0700

    Changed APerformance to perf in for loop in statement function, as it was not the same meaning

--- main.js
+++ main.js
@@ -128,14 +128,14 @@ function statement(invoice, plays)
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    for (let aPerformance of invoice.performances) 
+    for (let perf of invoice.performances) 
         {
-      volumeCredits += volumeCreditsFor(aPerformance);
+      volumeCredits += volumeCreditsFor(perf);
       // print line for this order
-      result += ` ${playFor(aPerformance).name}: ${usd(amountFor(aPerformance))} (${
-        aPerformance.audience
+      result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+        perf.audience
       } seats)\n`;
-      totalAmount += amountFor(aPerformance);
+      totalAmount += amountFor(perf);
     }
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
