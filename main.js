7b20be77ff66b254ca8a1a7087f4bf6c1565647e
Kobe Taylor <ktaylor2@linfield.edu>
2025-04-04 12:44:59 -0700

    One change - Renamed variable perf to aPerformance for clarity

--- main.js
+++ main.js
@@ -94,22 +94,22 @@ let invoice = require("./invoice.json")
 //    ]
 // }
 
-function amountFor(perf,play)
+function amountFor(aPerformance,play)
 {
   let thisAmount = 0;
   switch (play.type) {
     case 'tragedy':
       thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      if (aPerformance.audience > 30) {
+        thisAmount += 1000 * (aPerformance.audience - 30);
       }
       break;
     case 'comedy':
       thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      if (aPerformance.audience > 20) {
+        thisAmount += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      thisAmount += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
@@ -128,16 +128,16 @@ function statement(invoice, plays)
       currency: 'USD',
       minimumFractionDigits: 2,
     }).format;
-    for (let perf of invoice.performances) 
+    for (let aPerformance of invoice.performances) 
         {
-      const play = plays[perf.playID];
-      let thisAmount = amountFor(perf,play);
-      volumeCredits += Math.max(perf.audience - 30, 0);
+      const play = plays[aPerformance.playID];
+      let thisAmount = amountFor(aPerformance,play);
+      volumeCredits += Math.max(aPerformance.audience - 30, 0);
       // add extra credit for every ten comedy attendees
-      if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+      if ('comedy' === play.type) volumeCredits += Math.floor(aPerformance.audience / 5);
       // print line for this order
       result += ` ${play.name}: ${format(thisAmount / 100)} (${
-        perf.audience
+        aPerformance.audience
       } seats)\n`;
       totalAmount += thisAmount;
     }
